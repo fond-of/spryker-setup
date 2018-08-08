72f90191f20f3ad4c16b1010c504f5c5e95ec5b6
Daniel Rose <daniel-rose@gmx.de>
2018-08-08 15:35:29 +0200

    allow builds to keep in jenkins jobs

--- src/FondOfSpryker/Zed/Setup/Business/Model/Cronjobs.php
+++ src/FondOfSpryker/Zed/Setup/Business/Model/Cronjobs.php
@@ -7,6 +7,81 @@ use Spryker\Zed\Setup\Business\Model\Cronjobs as BaseCronjobs;
 
 class Cronjobs extends BaseCronjobs
 {
+    const DEFAULT_AMOUNT_OF_BUILDS_FOR_LOGFILE_ROTATION = -1;
+    const FALLBACK_AMOUNT_OF_DAYS_FOR_LOGFILE_ROTATION = -1;
+
+    /**
+     * Get number of builds to keep job output history. Each run is a directory, so we definitely need to keep it clean.
+     *
+     * @param array $job
+     *
+     * @return int
+     */
+    protected function getBuildsToKeep(array $job)
+    {
+        if (array_key_exists('logrotate_builds', $job) && is_int($job['logrotate_builds'])) {
+            return $job['logrotate_builds'];
+        }
+
+        return self::DEFAULT_AMOUNT_OF_BUILDS_FOR_LOGFILE_ROTATION;
+    }
+
+    /**
+     * Render Job description (as XML) for Jenkins API call
+     *
+     * @todo Move XML snippet to twig template
+     *
+     * @param array $job
+     *
+     * @return string
+     */
+    protected function prepareJobXml(array $job)
+    {
+        $disabled = ($job['enable'] === true) ? 'false' : 'true';
+        $schedule = $this->getSchedule($job);
+        $buildsToKeep = $this->getBuildsToKeep($job);
+        $daysToKeep = $this->getDaysToKeep($job);
+
+        if ($buildsToKeep !== self::DEFAULT_AMOUNT_OF_BUILDS_FOR_LOGFILE_ROTATION) {
+            $daysToKeep = self::FALLBACK_AMOUNT_OF_DAYS_FOR_LOGFILE_ROTATION;
+        }
+
+        $command = htmlspecialchars($job['command']);
+        $store = $job['store'];
+
+        $xml = "<?xml version='1.0' encoding='UTF-8'?>
+<project>
+  <actions/>
+  <description></description>
+  <logRotator>
+    <daysToKeep>$daysToKeep</daysToKeep>
+    <numToKeep>$buildsToKeep</numToKeep>
+    <artifactDaysToKeep>$daysToKeep</artifactDaysToKeep>
+    <artifactNumToKeep>$buildsToKeep</artifactNumToKeep>
+  </logRotator>
+  <keepDependencies>false</keepDependencies>
+  <properties/>
+  <scm class='hudson.scm.NullSCM'/>
+  <canRoam>true</canRoam>
+  <disabled>$disabled</disabled>
+  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+  <triggers class='vector'>$schedule</triggers>
+  <concurrentBuild>false</concurrentBuild>
+  <builders>
+    <hudson.tasks.Shell>";
+
+        $xml .= $this->getCommand($command, $store);
+        $xml .= "
+    </hudson.tasks.Shell>
+  </builders>\n"
+            . $this->getPublisherString($job) . "\n
+  <buildWrappers/>
+</project>\n";
+
+        return $xml;
+    }
+
     /**
      * @param string $command
      * @param string $store
--- tests/FondOfSpryker/Zed/Setup/Business/Model/CronjobsTest.php
+++ tests/FondOfSpryker/Zed/Setup/Business/Model/CronjobsTest.php
@@ -33,6 +33,178 @@ class CronjobsTest extends Unit
     /**
      * @return void
      */
+    public function testPrepareJobXmlWithDaysToKeep()
+    {
+        $expectedXml = "<?xml version='1.0' encoding='UTF-8'?>
+<project>
+  <actions/>
+  <description></description>
+  <logRotator>
+    <daysToKeep>5</daysToKeep>
+    <numToKeep>-1</numToKeep>
+    <artifactDaysToKeep>5</artifactDaysToKeep>
+    <artifactNumToKeep>-1</artifactNumToKeep>
+  </logRotator>
+  <keepDependencies>false</keepDependencies>
+  <properties/>
+  <scm class='hudson.scm.NullSCM'/>
+  <canRoam>true</canRoam>
+  <disabled>false</disabled>
+  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+  <triggers class='vector'></triggers>
+  <concurrentBuild>false</concurrentBuild>
+  <builders>
+    <hudson.tasks.Shell><command>
+export APPLICATION_ENV=" . APPLICATION_ENV . "
+export APPLICATION_STORE=UNIT
+cd " . APPLICATION_ROOT_DIR . "
+. ./config/Zed/cronjobs/cron.conf
+\$PHP_BIN vendor/bin/console product-label:relations:update -vvv</command>
+    </hudson.tasks.Shell>
+  </builders>
+<publishers/>
+
+  <buildWrappers/>
+</project>\n";
+
+        $job = [
+            'name' => 'update-product-label-relations',
+            'command' => '$PHP_BIN vendor/bin/console product-label:relations:update -vvv',
+            'schedule' => '',
+            'enable' => true,
+            'run_on_non_production' => true,
+            'logrotate_days' => 5,
+            'store' => 'UNIT',
+        ];
+
+        $reflection = new ReflectionClass(get_class($this->cronjobs));
+
+        $method = $reflection->getMethod('prepareJobXml');
+        $method->setAccessible(true);
+
+        $actualXml = $method->invokeArgs($this->cronjobs, [$job]);
+        $this->assertEquals($expectedXml, $actualXml);
+    }
+
+    /**
+     * @return void
+     */
+    public function testPrepareJobXmlWithBuildsToKeep()
+    {
+        $expectedXml = "<?xml version='1.0' encoding='UTF-8'?>
+<project>
+  <actions/>
+  <description></description>
+  <logRotator>
+    <daysToKeep>-1</daysToKeep>
+    <numToKeep>20</numToKeep>
+    <artifactDaysToKeep>-1</artifactDaysToKeep>
+    <artifactNumToKeep>20</artifactNumToKeep>
+  </logRotator>
+  <keepDependencies>false</keepDependencies>
+  <properties/>
+  <scm class='hudson.scm.NullSCM'/>
+  <canRoam>true</canRoam>
+  <disabled>false</disabled>
+  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+  <triggers class='vector'></triggers>
+  <concurrentBuild>false</concurrentBuild>
+  <builders>
+    <hudson.tasks.Shell><command>
+export APPLICATION_ENV=" . APPLICATION_ENV . "
+export APPLICATION_STORE=UNIT
+cd " . APPLICATION_ROOT_DIR . "
+. ./config/Zed/cronjobs/cron.conf
+\$PHP_BIN vendor/bin/console product-label:relations:update -vvv</command>
+    </hudson.tasks.Shell>
+  </builders>
+<publishers/>
+
+  <buildWrappers/>
+</project>\n";
+
+        $job = [
+            'name' => 'update-product-label-relations',
+            'command' => '$PHP_BIN vendor/bin/console product-label:relations:update -vvv',
+            'schedule' => '',
+            'enable' => true,
+            'run_on_non_production' => true,
+            'logrotate_builds' => 20,
+            'logrotate_days' => 7,
+            'store' => 'UNIT',
+        ];
+
+        $reflection = new ReflectionClass(get_class($this->cronjobs));
+
+        $method = $reflection->getMethod('prepareJobXml');
+        $method->setAccessible(true);
+
+        $actualXml = $method->invokeArgs($this->cronjobs, [$job]);
+        $this->assertEquals($expectedXml, $actualXml);
+    }
+
+    /**
+     * @return void
+     */
+    public function testPrepareJobXmlWithPriority()
+    {
+        $expectedXml = "<?xml version='1.0' encoding='UTF-8'?>
+<project>
+  <actions/>
+  <description></description>
+  <logRotator>
+    <daysToKeep>-1</daysToKeep>
+    <numToKeep>20</numToKeep>
+    <artifactDaysToKeep>-1</artifactDaysToKeep>
+    <artifactNumToKeep>20</artifactNumToKeep>
+  </logRotator>
+  <keepDependencies>false</keepDependencies>
+  <properties/>
+  <scm class='hudson.scm.NullSCM'/>
+  <canRoam>true</canRoam>
+  <disabled>false</disabled>
+  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+  <triggers class='vector'></triggers>
+  <concurrentBuild>false</concurrentBuild>
+  <builders>
+    <hudson.tasks.Shell><command>
+export APPLICATION_ENV=" . APPLICATION_ENV . "
+export APPLICATION_STORE=UNIT
+cd " . APPLICATION_ROOT_DIR . "
+. ./config/Zed/cronjobs/cron.conf
+\$PHP_BIN vendor/bin/console product-label:relations:update -vvv</command>
+    </hudson.tasks.Shell>
+  </builders>
+<publishers/>
+
+  <buildWrappers/>
+</project>\n";
+
+        $job = [
+            'name' => 'update-product-label-relations',
+            'command' => '$PHP_BIN vendor/bin/console product-label:relations:update -vvv',
+            'schedule' => '',
+            'enable' => true,
+            'run_on_non_production' => true,
+            'logrotate_builds' => 20,
+            'store' => 'UNIT',
+        ];
+
+        $reflection = new ReflectionClass(get_class($this->cronjobs));
+
+        $method = $reflection->getMethod('prepareJobXml');
+        $method->setAccessible(true);
+
+        $actualXml = $method->invokeArgs($this->cronjobs, [$job]);
+        $this->assertEquals($expectedXml, $actualXml);
+    }
+
+    /**
+     * @return void
+     */
     public function testGetCommand()
     {
         $store = 'UNIT';
